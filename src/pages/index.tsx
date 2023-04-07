39524df04db9203db1948346b54b13d915508409
Nathan Guy <nathanguy@Nathans-MacBook-Pro-3.local>
2023-04-06 22:06:44 -0500

    adding files in

---
+++ src/pages/index.tsx
@@ -0,0 +1,110 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+        
+        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+          
+            By MoneyDAO{' '}
+            
+        </div>
+      </div>
+
+      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] ">
+        <a href="./moneydao">
+          <Image
+            className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] "
+            src="/moneydao-logo.png"
+            alt="Next.js Logo"
+            width={180}
+            height={37}
+            priority
+          />
+        </a>
+      </div>
+
+      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
+        <a
+          href="https://moneyboys.io/"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
+            Site{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p
+            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
+          >
+            MoneyBoys website.
+          </p>
+        </a>
+
+        <a
+          href="https://twitter.com/MoneyBoysNFT"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
+            Twitter{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p
+            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
+          >
+            MoneyBoys Twitter.
+          </p>
+        </a>
+
+        <a
+          href="https://discord.com/invite/h2Mv5kKjgK"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
+            Discord{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p
+            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
+          >
+            Join the MoneyBoys Discord.
+          </p>
+        </a>
+
+        <a
+          href="https://magiceden.io/marketplace/solana_money_boys"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
+            Marketplace{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p
+            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
+          >
+            Don't have a MoneyBoy? Buy one here.
+          </p>
+        </a>
+      </div>
+    </main>
+  )
+}
---
+++ src/pages/moneydao.tsx
@@ -0,0 +1,12 @@
+import react from 'react';
+
+const MoneyDao = () => {
+    return (
+        <div>
+            <h1>MoneyDao</h1>
+            <p>The DAO targets to grow the Money Boy project, community and its relative value. The separate self-governed branch is funded to exercise what it views fit on behalf of the Money Boy community in tandem with the core team, to further mutual objectives.</p>
+        </div>
+    )
+};
+
+export default MoneyDao;
